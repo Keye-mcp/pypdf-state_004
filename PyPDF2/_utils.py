15a2be7166be999c532e8a076e46adbc967617a8
Hanna Toma <53424347+htoma@users.noreply.github.com>
2022-06-06 13:29:00 +0200

    STY: black, isort, Flake8, splitting buildCharMap (#950)
    This commit contains several small stylistic changes. Most of then were found by Flake8.
    
    The biggest single change is moving buildCharMap to its own module. There it was split into several functions to make it easier to understand how it works. This could also make it easier to understand adjustments in future pull requests.
    
    A caching mechanism for downloaded files was added to speed up local testing

--- PyPDF2/_utils.py
+++ PyPDF2/_utils.py
@@ -31,10 +31,10 @@ Utility functions for PDF library.
 __author__ = "Mathieu Fenniak"
 __author_email__ = "3604266+bzielinski@users.noreply.github.com"
 
+import warnings
 from codecs import getencoder
 from io import BufferedReader, BufferedWriter, BytesIO, FileIO
 from typing import Any, Dict, Optional, Tuple, Union, overload
-import warnings
 
 try:
     # Python 3.10+: https://www.python.org/dev/peps/pep-0484/
